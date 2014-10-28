e7a56634a77de0570358d3e3c589bd31a17e110a
shypre <shyprea2@gmail.com>
2014-10-27 23:21:48 -0700

    more stuff

--- c++ sources/factorialfinder.cpp
+++ c++ sources/factorialfinder.cpp
@@ -13,6 +13,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
 #include "stdafx.h"
 #include <iostream>
 using namespace std;
@@ -30,33 +31,6 @@ void findfactorial(__int64 num, __int64 factorialanswer)
 
 int main()
 {
-	/* char details;
-	cout << "Factorial Finder  Copyright (C) 2014  Michael Lin \n" << 
-		"This program comes with ABSOLUTELY NO WARRANTY; \n" <<
-		"for details type `w' and press enter. \n" <<
-		"This is free software, and you are welcome to \n" <<
-		"redistribute it under certain conditions; \n" <<
-		"type `c' and press enter for details. \n" <<
-		"To continue, type `y'."<< endl;
-license:
-	cin >> details;
-	cin.ignore();
-	if (details == 'w')
-	{
-		cout << "This program is free software: you can redistribute it and/or modify \n" <<
-				"it under the terms of the GNU General Public License as published by \n" <<
-				"the Free Software Foundation, either version 3 of the License, or \n" <<
-				"(at your option) any later version." << endl << endl;
-		goto license;
-	}
-	else if (details == 'c')
-	{
-		cout << " This program is distributed in the hope that it will be useful, \n" <<
-				"but WITHOUT ANY WARRANTY; without even the implied warranty of \n" <<
-				"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. \n" <<
-				"See the GNU General Public License for more details." << endl << endl;
-		goto license;
-	} */
 	__int64 num, factorialanswer = 1;
 	do
 	{
